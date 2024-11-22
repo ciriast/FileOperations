94f8fb65537a7790aed88bd5d29cfab60ac1c33b
ciriast <ciriastorres@gmail.com>
2024-11-22 06:02:27 -0600

    feat: Add a file to handle file operations with fcntl

---
+++ posix_operations.cpp
@@ -0,0 +1,11 @@
+#include <fcntl.h>
+
+// I'll be doing file operations with the fcntl file
+// It's part of the C standard library
+// It's for OS based on Unix
+
+// Let's check how to open a file using this API
+int openFileWithFcntl() {
+     
+    return 0
+}
